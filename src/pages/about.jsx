d1fb65f1a9131278a3dd07e76a3ca106f4efc448
Yuta Masuda <massu2000.1019@gmail.com>
2021-07-08 11:16:52 +0900

    pageに関するfile

---
+++ src/pages/about.jsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import styles from "src/styles/Home.module.css";
+import { Header } from "src/components/Header";
+import { Main } from "src/components/Main";
+import { Footer } from "src/components/Footer";
+
+export default function About(props) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>About Page</title>
+      </Head>
+      <Header />
+      {props.isShow ? <h1>{props.doubleCount}</h1> : null}
+      <button onClick={props.handleClick}>button</button>
+      <button onClick={props.handleDisplay}>
+        {props.isShow ? "非表示" : "表示"}
+      </button>
+      <input type="text" value={props.text} onChange={props.handleChange} />
+      <button onClick={props.handleAdd}>add</button>
+      <ul>
+        {props.array.map((item) => {
+          return <li key={item.id}>{item}</li>;
+        })}
+      </ul>
+      <Main page="about" />
+      <Footer />
+    </div>
+  );
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import classes from "src/styles/Home.module.css";
+import { Header } from "src/components/Header";
+import { Main } from "src/components/Main";
+import { Footer } from "src/components/Footer";
+
+export default function Index(props) {
+  return (
+    <div className={classes.container}>
+      <Head>
+        <title>Index Page</title>
+      </Head>
+      <Header />
+      {props.isShow ? <h1>{props.count}</h1> : null}
+      <button onClick={props.handleClick}>button</button>
+      <button onClick={props.handleDisplay}>{props.isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={props.text} onChange={props.handleChange} />
+      <button onClick={props.handleAdd}>add</button>
+      <ul>
+        {props.array.map((item) => {
+          return <li key={item.id}>{item}</li>;
+        })}
+      </ul>
+      <Main page="index" />
+      <Footer />
+    </div>
+  );
+}
