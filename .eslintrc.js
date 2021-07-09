31ea442bd407575b0bb16aa7d81e9afd2a9d3f74
Yuta Masuda <massu2000.1019@gmail.com>
2021-07-09 09:32:42 +0900

    🐛 eslintのsettings

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  parserOptions: {
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: { "react/prop-types": "off" },
+};
