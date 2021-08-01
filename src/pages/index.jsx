0e6f1856568b707774c6416363b814a9c3a10eba
Yuta Masuda <massu2000.1019@gmail.com>
2021-08-01 12:17:39 +0900

    🔥 indexから不要なcodeを削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,30 +1,16 @@
 import Head from "next/head";
 import styles from "src/styles/Home.module.css";
 import { Header } from "src/components/Header";
-import { Main } from "src/components/Main";
-import { Footer } from "src/components/Footer";
 
-export default function Index(props) {
+export default function Index() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      {props.isShow ? <h1>{props.doubleCount}</h1> : null}
-      <button onClick={props.handleClick}>button</button>
-      <button onClick={props.handleDisplay}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-      <input type="text" value={props.text} onChange={props.handleChange} />
-      <button onClick={props.handleAdd}>add</button>
-      <ul>
-        {props.array.map((item) => {
-          return <li key={item.id}>{item}</li>;
-        })}
-      </ul>
-      <Main page="index" />
-      <Footer />
+      <h1>Next.js</h1>
+      <p>Json placeholderを学ぼう</p>
     </div>
   );
 }
