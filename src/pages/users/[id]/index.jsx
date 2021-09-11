983d7edc1b58e4c424e2f1d9cc1b537dffdb83ba
Yuta Masuda <massu2000.1019@gmail.com>
2021-09-11 23:17:57 +0900

    ⚡️ SSRによってローディング非表示にした

--- src/pages/users/[id]/index.jsx
+++ src/pages/users/[id]/index.jsx
@@ -5,27 +5,33 @@ import { SWRConfig } from "swr";
 
 export const getServerSideProps = async (ctx) => {
   const { id } = ctx.query;
-  const API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
-  const user = await fetch(API_URL);
+  // ユーザー情報の取得
+  const USER_API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
+  const user = await fetch(USER_API_URL);
   const userData = await user.json();
+  // ユーザーの投稿の取得
+  const POSTS_API_URL = `https://jsonplaceholder.typicode.com/posts?userId=${userData.id}`;
+  const posts = await fetch(POSTS_API_URL);
+  const postsData = await posts.json();
 
   return {
     props: {
-      fallback:{
-        [API_URL]: userData
-      }
+      fallback: {
+        [USER_API_URL]: userData,
+        [POSTS_API_URL]: postsData,
+      },
     },
   };
 };
 
 const userId = (props) => {
-  const {fallback} = props;
+  const { fallback } = props;
   return (
     <div>
       <Head>
         <title>Users Page</title>
       </Head>
-      <SWRConfig value={{fallback}}>
+      <SWRConfig value={{ fallback }}>
         <Header />
         <UserComponent />
       </SWRConfig>
