2ab2b27d51e0fa7ff9d1c8bae6af4ab42a4cc6b1
Yuta Masuda <massu2000.1019@gmail.com>
2021-12-03 21:25:42 +0900

    ✨ ISRの練習（本番では無意味）

--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -1,12 +1,11 @@
 import Head from "next/head";
 import { CommentComponent } from "src/components/Comment";
 import { Header } from "src/components/Header";
+import { API_URL } from "src/utils/api";
 import { SWRConfig } from "swr";
 
 export const getStaticPaths = async () => {
-  const comments = await fetch(
-    "https://jsonplaceholder.typicode.com/comments?_limit=10"
-  );
+  const comments = await fetch(`${API_URL}/comments?_limit=10`);
   const commentsData = await comments.json();
   const paths = commentsData.map((comment) => {
     return {
@@ -21,15 +20,16 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async (ctx) => {
   const { id } = ctx.params;
-  const COMMENT_API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`;
+  const COMMENT_API_URL = `${API_URL}/comments/${id}`;
   const comment = await fetch(COMMENT_API_URL);
 
   if (!comment.ok) {
     return {
       notFound: true,
+      revalidate: 10
     };
   }
-  
+
   const commentData = await comment.json();
 
   return {
@@ -38,6 +38,7 @@ export const getStaticProps = async (ctx) => {
         [COMMENT_API_URL]: commentData,
       },
     },
+    revalidate: 10,
   };
 };
 
--- src/pages/comments/index.jsx
+++ src/pages/comments/index.jsx
@@ -1,10 +1,11 @@
 import Head from "next/head";
 import { CommentsComponent } from "src/components/Comments";
 import { Header } from "src/components/Header";
+import { API_URL } from "src/utils/api";
 import { SWRConfig } from "swr";
 
 export const getStaticProps = async () => {
-  const COMMENTS_API_URL = `https://jsonplaceholder.typicode.com/comments`;
+  const COMMENTS_API_URL = `${API_URL}/comments`;
   const comments = await fetch(COMMENTS_API_URL);
   const commentsData = await comments.json();
   
@@ -14,6 +15,7 @@ export const getStaticProps = async () => {
         [COMMENTS_API_URL]: commentsData,
       },
     },
+    revalidate: 10
   };
 };
 
